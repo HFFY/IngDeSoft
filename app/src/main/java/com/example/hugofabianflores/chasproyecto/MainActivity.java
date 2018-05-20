0b70f60678585af5a5c0856bc17ffb9ec41e79b7
HFFY <hugorilahffy@gmail.com>
2018-05-20 12:18:29 -0400

    aplicando firebase

--- app/src/main/java/com/example/hugofabianflores/chasproyecto/MainActivity.java
+++ app/src/main/java/com/example/hugofabianflores/chasproyecto/MainActivity.java
@@ -9,11 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hugo
-        int a;
-        int z=9;
-        int y=10;
-        int j=4;
-        a=1;
+
     }
 }
