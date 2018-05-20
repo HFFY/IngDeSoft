ec69ba37570231e32afaf52252a5648484a5251e
HFFY <hugorilahffy@gmail.com>
2018-05-20 10:59:18 -0400

    aplicando firebase

--- app/src/main/java/com/example/hugofabianflores/chasproyecto/Usuario.java
+++ app/src/main/java/com/example/hugofabianflores/chasproyecto/Usuario.java
@@ -3,17 +3,11 @@ package com.example.hugofabianflores.chasproyecto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Usuario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //hugo
-        int a;
-        int z=9;
-        int y=10;
-        int j=4;
-        a=1;
+        setContentView(R.layout.activity_usuario);
     }
 }
