849519772872623e59d9001ecef69e850e7cdecb
HFFY <hugorilahffy@gmail.com>
2018-05-21 23:51:20 -0400

    aplicando SQLServer

--- app/src/main/java/com/example/hugofabianflores/chasproyecto/MainActivity.java
+++ app/src/main/java/com/example/hugofabianflores/chasproyecto/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.hugofabianflores.chasproyecto;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.ConnectionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +20,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    public Connection conexionBD(){
+
+        Connection conexion = null;
+        try{
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            conexion = DriverManager.getConnection("jbdbc:jtds:sqlserver://ip;databasenName=;user=;password=;");
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+        return conexion;
+    }
+
+    public void AgregarUsuario(){
+        try {
+            PreparedStatement pst= conexionBD().prepareStatement("insert into  values (?,?)");
+            //pst.setString(1,edt.getText().toString);
+            pst.executeUpdate();
+            Toast.makeText(getApplicationContext(),"Registro exitoso",Toast.LENGTH_SHORT).show();
+        }catch (SQLException e){
+            Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
